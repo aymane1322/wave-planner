078f0174cb6fc1cd8f945a49293c50452520f96a
aymane1322 <aymane.elhadi@e-polytechnique.ma>
2024-03-10 19:55:07 +0000

    screen 1 finition

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,29 +1,69 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import Image from "next/image";
 import { headerData } from "../data/data";
 import { nanoid } from "nanoid";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { ImCross } from "react-icons/im";
 
 function Header() {
+  const [cliked, setCliked] = useState(true);
   return (
-    <header className="h-1/6 bg-black/40 flex justify-between items-center px-3">
-      <Image
-        className="rounded-md w-150 h-50 brightness-90 opacity-95 cursor-pointer"
+    <header className="h-1/6 bg-black/40 flex justify-between items-center px-3 ">
+      <div className=" w-24 h-9 sm:w-48 sm:h-16 brightness-90 opacity-95 cursor-pointer">
+      <Image className="rounded-md"
         src={"/logo.png"}
         alt="logo"
-        width={150}
-        height={100}
+        layout="fill"
       ></Image>
-      <div className="flex justify-between flex-wrap ">
-        <ul className="hidden md:flex">
+      </div>
+      <div className="flex justify-between relative ">
+        <ul className="hidden md:flex flex-wrap md:justify-end ">
           {headerData.titles.map((e) => (
-            <li className="px-2 mx-1 text-xl text-myWhite cursor-pointer bg-slate-700 rounded-sm py-1 hover:bg-slate-800" key={nanoid()}>{e}</li>
+            <li
+              className="px-2 mx-1 text-xl text-myWhite cursor-pointer bg-slate-700 rounded-sm py-1 my-1 hover:bg-slate-800"
+              key={nanoid()}
+            >
+              {e}
+            </li>
           ))}
         </ul>
-        <div className="flex pl-7">
-          <div className="text-blue-400 hover:text-blue-300 font-myFont underline  cursor-pointer text-lg font-semibold md:text-xl">Login  </div>
+
+        <div className="flex md:w-80  justify-end ">
+          <div className="text-blue-400 hover:text-blue-300 font-myFont underline  cursor-pointer text-md font-semibold md:text-xl">
+            Login{" "}
+          </div>
           <span className="text-white pt-[2px] px-1">/</span>
-          <div className="text-blue-400 hover:text-blue-300 font-myFont underline cursor-pointer text-lg font-semibold md:text-xl">Create Acount</div>
+          <div className="text-blue-400 hover:text-blue-300 font-myFont underline cursor-pointer text-md font-semibold md:text-xl">
+            Create Acount
+          </div>
         </div>
+
+        {cliked ? (
+          <GiHamburgerMenu
+            onClick={() => setCliked(!cliked)}
+            className="md:hidden text-3xl ml-3"
+          />
+        ) : (
+          <ImCross
+            onClick={() => setCliked(!cliked)}
+            className="md:hidden text-3xl ml-3"
+          />
+        )}
+        {cliked ?<></>:<div className="absolute  bg-slate-400 opacity-80 md:hidden top-[50px] left-[50px] z-50">
+
+        <ul className=" ">
+          {headerData.titles.map((e) => (
+            <li
+              className="px-2 mx-1 text-xl text-myWhite cursor-pointer bg-slate-700 rounded-sm py-1 my-1 hover:bg-slate-800"
+              key={nanoid()}
+            >
+              {e}
+            </li>
+          ))}
+        </ul>
+
+        </div>}
       </div>
     </header>
   );
