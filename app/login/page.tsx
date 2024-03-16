137f775af3f13089e6d8283316a43ecb52a72165
aymane1322 <aymane.elhadi@e-polytechnique.ma>
2024-03-16 20:56:31 +0000

    same

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import { IoIosArrowBack } from "react-icons/io";
 import { IoEye } from "react-icons/io5";
 import { IoEyeOff } from "react-icons/io5";
+import Link from 'next/link';
 
 function page() {
   return (
@@ -12,7 +13,9 @@ function page() {
     
     <div className='h-[25%] grid grid-cols-6 place-items-center justify-center '>
       {/* <Image className='-z-50 ' src={"/backGround.jpg"} alt='background' fill></Image> */}
+      <Link href={"/"}>
       <IoIosArrowBack className='col-start-1 text-myWhite text-3xl' />
+      </Link>
       <h1 className='text-myWhite font-myFont text-2xl font-bold text-center col-start-2 col-end-6 '>Wave Planner</h1>
     </div>
 
@@ -26,7 +29,7 @@ function page() {
               <p className='text-[9px] font-bold text-right w-full'>Forgot Password?</p>
               <button type='submit' className='text-myWhite text-sm font-myFont bg-myBlue w-[100%] p-2 rounded-2xl font-bold'>Login</button>
               <p className='text-myBlack text-[9px] font-myFont font-bold pt-3'>Or login with</p>
-              <button className='text-myBlack flex items-center justify-center text-sm font-myFont bg-white w-[100%] p-2 rounded-2xl font-bold'><Image  src={"/google_ic.svg"} alt='google icon' width={20} height={20}></Image> <span className='ml-1 text-[10px]'>Log In Using Google</span></button>
+              <button className='text-myBlack flex items-center justify-center text-sm font-myFont bg-slate-200 w-[100%] p-2 rounded-2xl font-bold'><Image  src={"/google_ic.svg"} alt='google icon' width={20} height={20}></Image> <span className='ml-1 text-[10px]'>Log In Using Google</span></button>
               <button className='text-myWhite flex items-center justify-center text-sm font-myFont bg-myBlack w-[100%] p-2 rounded-2xl font-bold'><Image  src={"/cib_apple.svg"} alt='google icon' width={20} height={20}></Image> <span className='ml-3 text-[10px]'>Log In Using Apple</span></button>
               <p className=' absolute  text-myBlack text-[12px] font-myFont font-bold   -bottom-20  '>Don’t have an account? <span className='text-myBlue cursor-pointer'>Register Now</span></p>
           </form>
