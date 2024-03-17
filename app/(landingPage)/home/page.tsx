55544892e662d377669efc1e81e3996ec2475d52
aymane1322 <aymane.elhadi@e-polytechnique.ma>
2024-03-17 22:42:35 +0000

    starting the work on landing page

--- app/(landingPage)/home/page.tsx
+++ app/(landingPage)/home/page.tsx
@@ -2,9 +2,9 @@ import React from 'react'
 
 function Home() {
   return (
-    <main>
-        <h1>landing page ... welcome meste potato</h1>
-    </main>
+    <div className='bg-zinc-900 h-[60%] w-full overflow-scroll rounded-t-[2.3rem] flex items-center justify-center'>
+        <p className='text-3xl text-white'>Coming Soon ...</p>
+    </div>
   )
 }
 
--- app/(landingPage)/layout.tsx
+++ app/(landingPage)/layout.tsx
@@ -1,12 +1,35 @@
 import React from 'react'
+import Image from 'next/image';
+import { IoMdNotificationsOutline } from "react-icons/io";
 
 function layout({children}:Readonly<{
     children: React.ReactNode;
   }>) {
   return (
-    <main>
+    <main className='w-full h-full bg-gradient-to-r from-blue-500 to-cyan-500'>
+        <div className='h-[20%] grid grid-cols-12 grid-rows-2 items-end gap-y-3 justify-center text-center'>
+            <div className='col-start-1 col-end-4 flex justify-evenly'>
+              <div ><Image src={"/Vector.svg"} alt='drawer icon' height={25} width={25}></Image></div>
+              <div  ><Image src={"/Vector2.svg"} alt='drawer icon' height={22} width={22}></Image></div>
+            </div>
+
+            <div className='col-start-4 col-end-10 text-white font-bold'>Wave Planner</div>
+
+            <div className='col-start-10 col-end-13 flex justify-evenly'>
+              <div ><IoMdNotificationsOutline className='text-white text-3xl' /></div>
+              <div className=' bg-myWhite rounded-full w-7 h-7'>rip</div>
+            </div>
+
+            <div className='col-start-1 row-start-2 col-end-6 flex justify-evenly items-start h-full '>
+              <div className='row-start-2 col-start-2 col-span-2 self-start'><Image src={"/navButton.svg"} alt='drawer icon' height={35} width={35}></Image></div>
+              <div className='row-start-2 col-start-4  col-span-2 self-start'><Image src={"/navButton1.svg"} alt='drawer icon' height={35} width={35}></Image></div>
+              <div className='row-start-2 col-start-6 col-span-2 self-start'><Image src={"/navButton2.svg"} alt='drawer icon' height={35} width={35}></Image></div>
+            </div>
+        </div>
         {children}
-        <div className='text-red-700 text-3xl'>this is shared data</div>
+        <div className='bg-green-600 h-[20%] w-full'>
+
+        </div>
     </main>
   )
 }
