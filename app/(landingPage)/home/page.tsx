47179a0ac48a08c43c64ba490278891ffaba8833
aymane1322 <aymane.elhadi@e-polytechnique.ma>
2024-03-18 21:39:04 +0000

    home page finision

--- app/(landingPage)/home/page.tsx
+++ app/(landingPage)/home/page.tsx
@@ -4,8 +4,16 @@ import BigEvent from "../../components/BigEvent";
 function Home() {
   return (
     <div className="bg-slate-200 h-[65%] w-full overflow-scroll rounded-t-[2.3rem] flex flex-col items-center">
-      <p className="text-sm pt-3 pb-2 font-bold text-black text-center">Hot events This week 🔥</p>
-      <BigEvent></BigEvent>
+      <p className="text-sm pt-3 pb-2 font-bold text-black text-center">
+        Hot events This week 🔥
+      </p>
+      <BigEvent
+        userName="Aymane"
+        date="Wed, 27 July"
+        title="Surf with Aymane"
+        adress="Banana Point 😋"
+        imgPath="/image.png"
+      ></BigEvent>
     </div>
   );
 }
--- app/components/BigEvent.tsx
+++ app/components/BigEvent.tsx
@@ -1,26 +1,37 @@
 import React from "react";
 import { IoLocationSharp } from "react-icons/io5";
 import Image from "next/image";
+import { FaUserAlt } from "react-icons/fa";
 
-function BigEvent() {
+type BigEvent = {
+    userName:string,
+    imgPath?: string,
+    date:string,
+    title:string,
+    adress:string,
+}
+
+function BigEvent({userName,imgPath,date,title,adress}:BigEvent) {
   return (
-  <div className="bg-myWhite w-[90%] min-h-[22rem] rounded-3xl flex flex-col justify-center items-center shadow-md relative">
+  <div className="bg-myWhite w-[90%] min-h-[22rem] rounded-3xl flex flex-col justify-center items-center shadow-md relative mb-2">
     
     <div className="w-[93%] h-[93%] rounded-3xl flex flex-col justify-between">
         {/* fill image go her ...🏄‍♀️ */}
         <Image className="object-cover rounded-3xl p-3 " src={"/event1.jpg"} alt="event image" fill></Image>
         <div className="z-10  w-full h-[11%] flex justify-between mt-3">
-            <div className="bg-myWhite w-[30%] ml-2 rounded-full h-[100%] flex items-center justify-center">
-                <div className="bg-gray-600 w-[1.7rem] h-[1.7rem] rounded-full">
+            <div className="bg-myWhite w-fit ml-2 rounded-full h-[100%] flex items-center justify-center">
+                <div className=" w-[1.6rem] h-[1.6rem] rounded-full ml-2 flex items-center justify-center overflow-hidden">
                     {/* profile image go her ...🙊 */}
+                    {/* <img src="/image.png" alt="user image" className="rounded-full object-cover" /> */}
+                    {!imgPath?<FaUserAlt />:<Image className="rounded-full" src={imgPath} alt="user image" width={30} height={30}></Image>}
                 </div>
-                <p className="text-black text-[0.7rem] font-bold pl-2">NUE</p>
+                <p className="text-black text-[0.7rem] font-bold pl-1 pr-2">{userName}</p>
             </div>
-            <div className="bg-myWhite w-[42%] mr-2 rounded-full h-[100%] text-black text-[0.7rem] font-bold flex justify-center items-center">Wed, 27 July</div>
+            <div className="bg-myWhite w-fit px-4 mr-2 rounded-full h-[100%] text-black text-[0.7rem] font-bold flex justify-center items-center">{date}</div>
         </div>
         <div className=" w-full h-[35%] z-10">
-            <h2 className="text-myWhite text-md font-bold font-sans pl-2">Surf with NUE</h2>
-            <p className="flex"><IoLocationSharp className="text-myBlue pl-2 text-2xl" /><span className="text-myWhite text-md  font-myFont">Balagan beach</span></p>
+            <h2 className="text-myWhite text-md font-bold font-sans pl-2">{title}</h2>
+            <p className="flex"><IoLocationSharp className="text-myBlue pl-2 text-2xl" /><span className="text-myWhite text-md  font-myFont">{adress}</span></p>
             <div className="w-full flex justify-center">
                 <button className="text-myWhite text-sm font-bold font-sans bg-myBlue px-7 py-3 rounded-full mt-3 ">Join!</button>
             </div>
