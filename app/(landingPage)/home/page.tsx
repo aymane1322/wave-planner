a5e2ad997536b43e2bdc060255f60ca90826b4b8
aymane1322 <aymane.elhadi@e-polytechnique.ma>
2024-03-18 18:59:12 +0000

    landing page finished

--- app/(landingPage)/home/page.tsx
+++ app/(landingPage)/home/page.tsx
@@ -3,11 +3,9 @@ import BigEvent from "../../components/BigEvent";
 
 function Home() {
   return (
-    <div className="bg-slate-200 h-[65%] w-full overflow-scroll rounded-t-[2.3rem] ">
+    <div className="bg-slate-200 h-[65%] w-full overflow-scroll rounded-t-[2.3rem] flex flex-col items-center">
       <p className="text-sm pt-3 font-bold text-black text-center">Hot events This week 🔥</p>
-      <BigEvent>
-        
-      </BigEvent>
+      <BigEvent></BigEvent>
     </div>
   );
 }
--- app/components/BigEvent.tsx
+++ app/components/BigEvent.tsx
@@ -1,11 +1,35 @@
-import React from 'react'
+import React from "react";
+import { IoLocationSharp } from "react-icons/io5";
+import Image from "next/image";
 
 function BigEvent() {
   return (
-    <div>
-        Big Event
+  <div className="bg-myWhite w-[90%] min-h-[22rem] rounded-3xl flex flex-col justify-center items-center shadow-md relative">
+    
+    <div className="w-[93%] h-[93%] rounded-3xl flex flex-col justify-between">
+        {/* fill image go her ...🏄‍♀️ */}
+        <Image className="object-cover rounded-3xl p-2 " src={"/event1.jpg"} alt="event image" fill></Image>
+        <div className="z-10  w-full h-[11%] flex justify-between mt-2">
+            <div className="bg-myWhite w-[30%] ml-2 rounded-full h-[100%] flex items-center justify-evenly">
+                <div className="bg-gray-600 w-[1.7rem] h-[1.7rem] rounded-full">
+                    {/* profile image go her ...🙊 */}
+                </div>
+                <p className="text-black text-[0.7rem] font-bold">NUE</p>
+            </div>
+            <div className="bg-myWhite w-[42%] mr-2 rounded-full h-[100%] text-black text-[0.7rem] font-bold flex justify-center items-center">Wed, 27 July</div>
+        </div>
+        <div className=" w-full h-[35%] z-10">
+            <h2 className="text-myWhite text-md font-bold font-sans pl-2">Surf with NUE</h2>
+            <p className="flex"><IoLocationSharp className="text-myBlue pl-2 text-2xl" /><span className="text-myWhite text-md  font-myFont">Balagan beach</span></p>
+            <div className="w-full flex justify-center">
+                <button className="text-myWhite text-sm font-bold font-sans bg-myBlue px-7 py-3 rounded-full mt-3 ">Join!</button>
+            </div>
+        </div>
+        {/* more decoration ... */}
+        <div className="absolute bg-myWhite w-2 h-[88%] rounded-r-full -right-1 top-5 shadow-md"></div>
+        <div className="absolute bg-myWhite w-2 h-[70%] rounded-r-3xl -right-2 top-14 shadow-md"></div>
     </div>
-  )
+  </div>);
 }
 
-export default BigEvent
+export default BigEvent;
