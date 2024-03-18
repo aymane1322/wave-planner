842913d6d190213793ffdf40e29b2c591b28928d
aymane1322 <aymane.elhadi@e-polytechnique.ma>
2024-03-18 17:03:09 +0000

    finishing the header and the footer of the home page

--- app/(landingPage)/home/page.tsx
+++ app/(landingPage)/home/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Home() {
   return (
-    <div className='bg-zinc-900 h-[60%] w-full overflow-scroll rounded-t-[2.3rem] flex items-center justify-center '>
+    <div className='bg-zinc-900 h-[65%] w-full overflow-scroll rounded-t-[2.3rem] flex items-center justify-center '>
         <p className='text-3xl text-white'>Coming Soon ...</p>
     </div>
   )
