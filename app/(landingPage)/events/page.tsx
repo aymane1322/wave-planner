b656a74023993e3b49294f816aa7e883b7a4223c
aymane <aymane.elhadi@e-polytechnique.ma>
2024-03-19 13:10:39 +0000

    reusable components + start the event page

--- app/(landingPage)/events/page.tsx
+++ app/(landingPage)/events/page.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
+import HomeHeader from "../../components/HomeHeader";
 
 function Events() {
   return (
-    <div className=''>
-      Events
-      </div>
-  )
+    <>
+      <HomeHeader height="10%" gridRows="1" items="center"></HomeHeader>
+      <div className="h-[75%] bg-red-400"></div>
+    </>
+  );
 }
 
-export default Events
+export default Events;
