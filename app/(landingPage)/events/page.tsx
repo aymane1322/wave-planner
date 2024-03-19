d46757c2b80093aa15d0bef18c3a5e1495c7beea
aymane <aymane.elhadi@e-polytechnique.ma>
2024-03-19 14:10:24 +0000

    starting the event page

--- app/(landingPage)/events/page.tsx
+++ app/(landingPage)/events/page.tsx
@@ -1,11 +1,30 @@
 import React from "react";
 import HomeHeader from "../../components/HomeHeader";
+import Image from "next/image";
 
 function Events() {
   return (
     <>
+      <div className="py-4">
       <HomeHeader height="10%" gridRows="1" items="center"></HomeHeader>
-      <div className="h-[75%] bg-red-400"></div>
+      </div>
+      <div className="h-[75%]  flex flex-col items-center relative">
+        <div className="h-[50%] w-[90%] bg-yellow-400 rounded-3xl absolute ">
+          <Image
+            className="rounded-3xl"
+            fill
+            objectFit="cover"
+            src={"/eventPict.jpg"}
+            alt="event img"
+          ></Image>
+          <p className="absolute">text on top1</p>
+          <p className="">text on top2</p>
+          <p className="">text on top3</p>
+        </div>
+        <p className="text-myWhite text-[0.6rem] text-center pt-3 pb-2">
+          Events nearby
+        </p>
+      </div>
     </>
   );
 }
